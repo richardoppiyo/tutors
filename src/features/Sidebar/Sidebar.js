191eff6d929a38acca282643a5b100df93286dfd
Richard Opiyo <richardoppiyo@gmail.com>
2022-11-15 08:56:58 +0300

    basic set up done

---
+++ src/features/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Sidebar.css';
+
+const Sidebar = () => (
+    <div className="sidebar-div">
+        <h1>This is the Sidebar</h1>
+    </div>
+
+)
+
+export default Sidebar;
