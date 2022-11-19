a5f3b38c456a6aba48d56b6440ada609e26dacaa
Richard Opiyo <richardoppiyo@gmail.com>
2022-11-19 13:20:35 +0300

    add main page details

--- src/features/Main/Main.js
+++ src/features/Main/Main.js
@@ -1,10 +1,102 @@
 import React from "react";
+import { BsPersonCircle } from 'react-icons/bs';
 import './Main.css';
+import { AiFillStar, AiOutlineStar,
+  } from 'react-icons/ai';
+
 
 const Main = () => (
 
     <div className="main-div">
-        <h1>This is the main page</h1>
+        <div className="topdiv">
+             <p>Top part</p>
+             <BsPersonCircle className="abcde" />
+        </div>
+        <div>
+        <input type="text" placeholder="Search.."></input>
+        </div>
+        <div>
+            <h3>Online Tutors</h3>
+            <div className="grid-container">
+                <div className="details">
+                    <div className="detailsA">
+                        <div className="detailsATop">
+                            <BsPersonCircle className="mypic" /> 
+                            <div>
+                                <p>Name Here</p>
+                                <p>Country Here</p>
+                            </div>  
+                        </div>
+                    <p>dot</p>
+                    </div>
+                    <div className="detailsB">
+                        <p className="detailsBp">1000+ Teachings</p>
+                    </div>
+                    <div className="detailsC">
+                        <p className="detailsCp">Call Now</p>
+                        <div className="starbox">
+                            <AiFillStar className="stars" />
+                            <AiFillStar className="stars" />
+                            <AiFillStar className="stars" />
+                            <AiFillStar className="stars" />
+                            <AiOutlineStar className="stars" />
+                        </div>
+                    </div>
+    
+                </div>
+                <div className="details">
+                    <div className="detailsA">
+                        <div className="detailsATop">
+                            <BsPersonCircle className="mypic" /> 
+                            <div>
+                                <p>Name Here</p>
+                                <p>Country Here</p>
+                            </div>  
+                        </div>
+                    <p>dot</p>
+                    </div>
+                    <div className="detailsB">
+                        <p className="detailsBp">1000+ Teachings</p>
+                    </div>
+                    <div className="detailsC">
+                        <p className="detailsCp">Call Now</p>
+                        <div className="starbox">
+                            <AiFillStar className="stars" />
+                            <AiFillStar className="stars" />
+                            <AiFillStar className="stars" />
+                            <AiFillStar className="stars" />
+                            <AiOutlineStar className="stars" />
+                        </div>
+                    </div>
+                </div>
+                <div className="details">
+                    <div className="detailsA">
+                        <div className="detailsATop">
+                            <BsPersonCircle className="mypic" /> 
+                            <div>
+                                <p>Name Here</p>
+                                <p>Country Here</p>
+                            </div>  
+                        </div>
+                    <p>dot</p>
+                    </div>
+                    <div className="detailsB">
+                        <p className="detailsBp">1000+ Teachings</p>
+                    </div>
+                    <div className="detailsC">
+                        <p className="detailsCp">Call Now</p>
+                        <div className="starbox">
+                            <AiFillStar className="stars" />
+                            <AiFillStar className="stars" />
+                            <AiFillStar className="stars" />
+                            <AiFillStar className="stars" />
+                            <AiOutlineStar className="stars" />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
     
 )
--- src/features/Sidebar/Sidebar.js
+++ src/features/Sidebar/Sidebar.js
@@ -1,11 +1,71 @@
 import React from "react";
 import './Sidebar.css';
-
+// import {
+//      NavLink
+//   } from 'react-router-dom';
+//   import Nav from 'react-bootstrap/Nav';
+//   import Navbar from 'react-bootstrap/Navbar';
+  
 const Sidebar = () => (
     <div className="sidebar-div">
         <h1>This is the Sidebar</h1>
+        <div>
+            <h3>Tutors</h3>
+            <h3>Explore Languages</h3>
+            <h3>My Lessons</h3>
+            <h3>Statistics</h3>
+            <h3>Shopping Cart</h3>
+        </div>
     </div>
 
 )
 
 export default Sidebar;
+
+
+
+// const Sidebar = () => {
+    // const navigate = useNavigate();
+    // const dispatch = useDispatch();
+    
+    // const [showSidebar, setShowSidebar] = useState(false);
+    // const location = useLocation();
+  
+    // const navActive = ({ isActive }) => (isActive ? { backgroundColor: '#96bf01', color: 'white' } : {});
+  
+    // const navLinkHelper = ({
+    //   href, children, onClick,
+    // }) => (
+    //   <NavLink className="link-item" style={navActive} to={href} onClick={onClick}>
+    //     {children}
+    //   </NavLink>
+    // );
+  
+    // const navLinks = () => (
+    //   <>
+    //     <Nav.Link href="/" as={navLinkHelper}>
+    //       LAWYERS
+    //     </Nav.Link>
+    //     <Nav.Link href="/account" as={navLinkHelper}>
+    //     <Nav.Link href="/reserve" as={navLinkHelper}>
+    //       BOOK A LAWYER
+    //     </Nav.Link>
+    //     <Nav.Link href="/reservations" as={navLinkHelper}>
+    //       MY RESERVATIONS
+    //     </Nav.Link>
+    //       MY ACCOUNT
+    //     </Nav.Link>
+    //   </>
+    // );
+
+
+    // return (
+    //     <Nav className="navbar-links position-relative flex-column">
+    //               {navLinks()}
+    //             </Nav>
+       
+    //   );
+    // };
+    
+    // export default Sidebar;
+    
